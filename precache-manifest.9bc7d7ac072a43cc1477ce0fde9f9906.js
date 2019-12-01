cb9b6b48cbb8907321e42a066cfaa66f34ab666e
Sebastian <sebastianmar00@gmail.com>
2019-12-01 13:25:57 +0100

    Updates

---
+++ precache-manifest.9bc7d7ac072a43cc1477ce0fde9f9906.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97c325f0f96c5bfe3a23599465431a33",
+    "url": "/oppgave-2/index.html"
+  },
+  {
+    "revision": "a6254eb20f5ee73555ff",
+    "url": "/oppgave-2/static/css/main.d869a76a.chunk.css"
+  },
+  {
+    "revision": "dfa61687c0990d0021ee",
+    "url": "/oppgave-2/static/js/2.92181a1f.chunk.js"
+  },
+  {
+    "revision": "a6254eb20f5ee73555ff",
+    "url": "/oppgave-2/static/js/main.6e0b3cf7.chunk.js"
+  },
+  {
+    "revision": "6b32008d1eabdd1d6859",
+    "url": "/oppgave-2/static/js/runtime-main.f1a40d71.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/oppgave-2/static/media/logo.25bf045c.svg"
+  }
+]);
